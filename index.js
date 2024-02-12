c52eba213af8aa06bf8a3d4754b1a304210234bb
Tydric2406 <144037940+Tydric2406@users.noreply.github.com>
2024-02-12 12:04:52 +0100

    Update index.js

--- index.js
+++ index.js
@@ -30,24 +30,7 @@ var status2 = 1;  // 1 - online, 7 - invisible
 user2 = new steamUser();
 user2.logOn({"accountName": username2, "password": password2, "twoFactorCode": steamTotp.generateAuthCode(shared_secret2)});
 user2.on('loggedOn', () => {
-	if (user.steamID != null) console.log(user2.steamID + ' - Successfully logged on');
+	if (user2.steamID != null) console.log(user2.steamID + ' - Successfully logged on');
 	user2.setPersona(status2);               
 	user2.gamesPlayed(games2);
 });
-
-
-// var username2 = process.env.username2;
-// var password2 = process.env.password2;
-// var shared_secret2 = process.env.shared2;
-
-// var games2 = [1085660, 2073850];  // Enter here AppIDs of the needed games
-// var status2 = 1;  // 1 - online, 7 - invisible
-
-
-// user2 = new steamUser();
-// user2.logOn({"accountName": username2, "password": password2, "twoFactorCode": steamTotp.generateAuthCode(shared_secret2)});
-// user2.on('loggedOn', () => {
-// 	if (user2.steamID != null) console.log(user2.steamID + ' - Successfully logged on');
-// 	user2.setPersona(status2);               
-// 	user2.gamesPlayed(games2);
-// });
